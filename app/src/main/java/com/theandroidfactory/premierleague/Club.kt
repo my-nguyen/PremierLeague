812b99ea06c8f85dadad1f27be4c5b3e8df34e1b
Mỹ Nguyễn <mynguyen@Mys-MacBook-Pro.local>
2023-03-04 18:39:41 -0800

    Episode 2.0

---
+++ app/src/main/java/com/theandroidfactory/premierleague/Club.kt
@@ -0,0 +1,10 @@
+package com.theandroidfactory.premierleague
+
+data class Club(
+    val id: String = "",
+    val imageResId: Int = 0,
+    val title: String = "",
+    val description: String = "",
+    val descriptionLong: String = "",
+    val buttonText: String = ""
+)
---
+++ app/src/main/java/com/theandroidfactory/premierleague/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.theandroidfactory.premierleague
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.theandroidfactory.premierleague.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
