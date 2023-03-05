1f92eca1d9545cd6b3aee977503ef72f853f92eb
Mỹ Nguyễn <mynguyen@Mys-MacBook-Pro.local>
2023-03-04 20:27:05 -0800

    Episode 2.1

--- app/src/main/java/com/theandroidfactory/premierleague/Club.kt
+++ app/src/main/java/com/theandroidfactory/premierleague/Club.kt
@@ -2,9 +2,10 @@ package com.theandroidfactory.premierleague
 
 data class Club(
     val id: String = "",
-    val imageResId: Int = 0,
     val title: String = "",
     val description: String = "",
     val descriptionLong: String = "",
-    val buttonText: String = ""
+    val buttonText: String = "",
+    val imageResId: Int = 0,
+    val imageUrl: String = ""
 )
--- app/src/main/java/com/theandroidfactory/premierleague/MainActivity.kt
+++ app/src/main/java/com/theandroidfactory/premierleague/MainActivity.kt
@@ -9,5 +9,85 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val clubs = getClubs()
+    }
+
+    private fun getClubs(): List<Club> {
+        return mutableListOf<Club>().apply {
+            add(
+                Club(
+                    "arsenal",
+                    "Arsenal",
+                    "Description of the club",
+                    "A longer description of the club that wouldn't fit on a",
+                    "Learn more",
+                    R.drawable.arsenal,
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F707909635156348949%2F&psig=AOvVaw3LD-FAMIEWSrHUuNjvHtKx&ust=1678072761553000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCNjbzajqw_0CFQAAAAAdAAAAABAE"
+                )
+            )
+            add(
+                Club(
+                    "man_city",
+                    "Manchester City",
+                    "Description of the club",
+                    "A longer description of the club that wouldn't fit on a",
+                    "Learn more",
+                    R.drawable.man_city,
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Fmanchester-city-twitter-header--707909635156348947%2F&psig=AOvVaw1oW573U6OyYJtDhCggcTJ6&ust=1678072656784000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCMDMxPbpw_0CFQAAAAAdAAAAABAE"
+                )
+            )
+            add(
+                Club(
+                    "man_united",
+                    "Manchester United",
+                    "Description of the club",
+                    "A longer description of the club that wouldn't fit on a",
+                    "Learn more",
+                    R.drawable.man_united,
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Fmanchester-united-twitter-header--526499012685938517%2F&psig=AOvVaw3R0K3M80JsOJbXCa2piAO0&ust=1678072099509000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPD47ovqw_0CFQAAAAAdAAAAABAE"
+                )
+            )
+            add(
+                Club(
+                    "tottenham",
+                    "Tottenham Hotspur",
+                    "Description of the club",
+                    "A longer description of the club that wouldn't fit on a",
+                    "Learn more",
+                    R.drawable.tottenham,
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Ff-edits.tumblr.com%2Fpost%2F151100205100%2Ftottenham-hotspur-twitter-header&psig=AOvVaw0Y8J1XHoWxDlsmts04vnRI&ust=1678072810565000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCOjE_b_qw_0CFQAAAAAdAAAAABAE"
+                )
+            )
+            add(
+                Club(
+                    "chelsea",
+                    "Chelsea",
+                    "Description of the club",
+                    "A longer description of the club that wouldn't fit on a",
+                    "Learn more",
+                    R.drawable.chelsea,
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Ff-edits.tumblr.com%2Fpost%2F150435158577%2Fchelsea-twitter-header&psig=AOvVaw3YlI4FrFIcLlB9ZNw_k1te&ust=1678072837414000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPC75szqw_0CFQAAAAAdAAAAABAE"
+                )
+            )
+            add(
+                Club(
+                    "leicester_city",
+                    "Leicester City",
+                    "Description of the club",
+                    "A longer description of the club that wouldn't fit on a",
+                    "Learn more",
+                    R.drawable.leicester_city,
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftwitter.com%2FF_Edits%2Fstatus%2F773110666102267904&psig=AOvVaw09E-NjBhKa-s2GLIMJ--eS&ust=1678072935728000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKjZ1Pvqw_0CFQAAAAAdAAAAABAE"
+                )
+            )
+        }
     }
 }
+
+// man united: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Fmanchester-united-twitter-header--526499012685938517%2F&psig=AOvVaw3R0K3M80JsOJbXCa2piAO0&ust=1678072099509000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPD47ovqw_0CFQAAAAAdAAAAABAE
+// man city: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Fmanchester-city-twitter-header--707909635156348947%2F&psig=AOvVaw1oW573U6OyYJtDhCggcTJ6&ust=1678072656784000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCMDMxPbpw_0CFQAAAAAdAAAAABAE
+// arsenal: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F707909635156348949%2F&psig=AOvVaw3LD-FAMIEWSrHUuNjvHtKx&ust=1678072761553000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCNjbzajqw_0CFQAAAAAdAAAAABAE
+// tottenham: https://www.google.com/url?sa=i&url=https%3A%2F%2Ff-edits.tumblr.com%2Fpost%2F151100205100%2Ftottenham-hotspur-twitter-header&psig=AOvVaw0Y8J1XHoWxDlsmts04vnRI&ust=1678072810565000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCOjE_b_qw_0CFQAAAAAdAAAAABAE
+// chelsea: https://www.google.com/url?sa=i&url=https%3A%2F%2Ff-edits.tumblr.com%2Fpost%2F150435158577%2Fchelsea-twitter-header&psig=AOvVaw3YlI4FrFIcLlB9ZNw_k1te&ust=1678072837414000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPC75szqw_0CFQAAAAAdAAAAABAE
+// leicester city: https://www.google.com/url?sa=i&url=https%3A%2F%2Ftwitter.com%2FF_Edits%2Fstatus%2F773110666102267904&psig=AOvVaw09E-NjBhKa-s2GLIMJ--eS&ust=1678072935728000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKjZ1Pvqw_0CFQAAAAAdAAAAABAE
