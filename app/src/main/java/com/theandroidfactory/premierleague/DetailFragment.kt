3e96a5765d678f7347ca705e5015b2749152e7fb
Mỹ Nguyễn <mynguyen@Mys-MacBook-Pro.local>
2023-03-06 12:03:14 -0800

    Episode 2.10

---
+++ app/src/main/java/com/theandroidfactory/premierleague/DetailFragment.kt
@@ -0,0 +1,11 @@
+package com.theandroidfactory.premierleague
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class DetailFragment: Fragment(R.layout.fragment_detail) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/theandroidfactory/premierleague/MainFragment.kt
@@ -0,0 +1,11 @@
+package com.theandroidfactory.premierleague
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class MainFragment: Fragment(R.layout.fragment_main) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
